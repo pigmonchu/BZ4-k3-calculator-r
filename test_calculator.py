11dd2867dd5f78dee48cd8b78341cb862d9f2c21
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-12-13 17:55:32 +0100

    Inicio de versión final

--- test_calculator.py
+++ test_calculator.py
@@ -31,7 +31,10 @@ class CalculatorTests(TestCase):
         
     
 
-
+    def test_contar_parentesis(self):
+        self.assertEqual(openParenthesis('()'), 0)
+        self.assertEqual(openParenthesis('(((VI))'), 1)
+        self.assertEqual(openParenthesis('((VI))(('),2)
 
 if __name__ == '__main__':
     main()
